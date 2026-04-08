8617c586c750619d63a4fe9bf711d6db3ea40604
cqz <hisoncai@gmail.com>
2026-04-08 17:29:39 +0800

    更新2026-4-8

--- themes/hexo/components/Announcement.js
+++ themes/hexo/components/Announcement.js
@@ -1,10 +1,23 @@
 import { useGlobal } from '@/lib/global'
 import dynamic from 'next/dynamic'
+import { useEffect } from 'react'
 
 const NotionPage = dynamic(() => import('@/components/NotionPage'))
 
 const Announcement = ({ post, className }) => {
   const { locale } = useGlobal()
+  
+    // 豆包加上，自动让公告里的所有链接在新标签页打开
+  useEffect(() => {
+    const links = document.querySelectorAll('#announcement-content a')
+    links.forEach(link => {
+      link.target = '_blank'
+      link.rel = 'noreferrer noopener'
+    })
+  }, [post])
+  
+  
+  
   if (post?.blockMap) {
     return <div className={className}>
         <section id='announcement-wrapper' className="dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray">
