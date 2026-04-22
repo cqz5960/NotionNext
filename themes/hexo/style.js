d68d844ff98f2fa44ea8199983b35c8edd6a8cb8
cqz <hisoncai@gmail.com>
2026-04-22 14:08:16 +0800

    2026-04-22

--- themes/hexo/style.js
+++ themes/hexo/style.js
@@ -9,7 +9,7 @@ import CONFIG from './config'
  */
 const Style = () => {
   // 从配置中获取主题色，如果没有配置则使用默认值 #928CEE
-  const themeColor = siteConfig('HEXO_THEME_COLOR', '#556B2F', CONFIG) // 改颜色 by Hison
+  const themeColor = siteConfig('HEXO_THEME_COLOR', '#928CEE', CONFIG) 
 
   return (
     <style jsx global>{`
